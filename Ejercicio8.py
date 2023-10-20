2cf225c699f8eafde27fc2d707d47117e5cebba3
idomingexp <144793269+idomingexp@users.noreply.github.com>
2023-10-20 10:39:07 +0200

    Update Ejercicio8.py

--- Ejercicio8.py
+++ Ejercicio8.py
@@ -1,3 +1,4 @@
-a = input("Dime el precio del producto:")
-print("el producto vale {} € y {} céntimos".format(a[0:-3], a[-2:]))
+a = round(float(input("Dime el precio del producto:")),2)
+b = str(a)
+print("el producto vale {} € y {} céntimos".format(b[0:-3], b[-2:]))
 input()
